7d7ef12fc24da4b9749e85371817fb0002e958b3
wshuang6 <seraph617@gmail.com>
2017-04-03 19:57:10 -0400

    code for gform and response

--- src/components/gform/index.js
+++ src/components/gform/index.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './index.css';
+
+export default function Gform (props) {
+  const guessNum = 2;
+  return (
+    <div className="gform">
+      <form>
+        <input type="text" name="guess" placeholder="Enter a number" />
+        <button className="butt">
+          <span>Guess #{guessNum}!</span>
+        </button>
+      </form>
+    </div>
+  );
+}
--- src/components/response/index.js
+++ src/components/response/index.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './index.css';
+
+export default function Response (props) {
+  const text = "cold";
+  return (
+    <div className="response">
+      <p>
+        {text}
+      </p>
+    </div>
+  );
+}
